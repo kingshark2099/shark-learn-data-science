c635ea96c204949df8d073a17b13d5792539c107
Shark <109160242+kingshark2099@users.noreply.github.com>
2022-07-12 23:52:09 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello from Binder!")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# git fetch && git merge origin/main
+watched = pd.read_csv('watched.csv')
+watchlist = pd.read_csv('watchlist.csv')
+diary = pd.read_csv('diary.csv')
+ratings = pd.read_csv('ratings.csv')
