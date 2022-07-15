c82f4fcaa077a51469b89421f987ae30eb8b5ec1
Shark <109160242+kingshark2099@users.noreply.github.com>
2022-07-15 22:17:02 +0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,8 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-# git fetch && git merge origin/main
-
 # object creation
 fav_movies = pd.Series(["Interstellar", 
                         "Little Women",
