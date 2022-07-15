fb5fc0405c358acfd8f30f66878b8ee4ccefa1d4
Shark <109160242+kingshark2099@users.noreply.github.com>
2022-07-15 22:16:24 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-# git fetch && git merge origin/main
-
 # read data
 watched = pd.read_csv('watched.csv')
 watchlist = pd.read_csv('watchlist.csv')
